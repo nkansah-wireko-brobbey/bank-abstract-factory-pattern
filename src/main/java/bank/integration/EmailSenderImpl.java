b61603e87e000c354df9c4908328b95107e7151c
Nkansah <nkansahwirekobrobbey@gmail.com>
2025-04-20 01:37:00 -0500

    Implement singleton pattern

--- src/main/java/bank/integration/EmailSenderImpl.java
+++ src/main/java/bank/integration/EmailSenderImpl.java
@@ -4,10 +4,23 @@ import bank.domain.Customer;
 
 public class EmailSenderImpl implements IEmailSender {
 
+    private static volatile EmailSenderImpl instance;
+
+    private EmailSenderImpl(){
+
+    }
 
     @Override
     public void sendEmail(Customer customer) {
         System.out.println("Hi "+customer.getName()+", Changes has been made to your account.");
+    }
+
+    public static EmailSenderImpl getInstance(){
+        if (instance == null){
+            instance = new EmailSenderImpl();
+        }
+
+        return instance;
     }
     
 }
